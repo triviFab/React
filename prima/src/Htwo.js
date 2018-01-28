d5b7a215de35077d9f6c6f851accbb8955a2c8ad
triviFab <fabio.trivisonno@gmail.com>
2018-01-28 19:23:02 +0100

    react training

---
+++ prima/src/Htwo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Htwo extends Component {
+    state = {}
+    render() { 
+        return ( 
+            <h2>{this.props.value}</h2>
+         );
+    }
+}
+ 
+
+export default Htwo;
